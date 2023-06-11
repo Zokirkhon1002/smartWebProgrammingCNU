8227a824291ec829dbe8619e8217251e9613212c
Zokirkhon1002 <zokirxonkotibxanov@gmail.com>
2023-06-12 03:21:06 +0900

    homework done

---
+++ 23-lesson/homework/js/layout.js
@@ -0,0 +1,7 @@
+// @ts-nocheck
+const footerDate = document.getElementById("footerDate");
+
+// DATE FOR FOOTER START
+const date = new Date();
+footerDate.innerText = date.getFullYear();
+// DATE FOR FOOTER END
